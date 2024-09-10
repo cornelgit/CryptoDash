70533d6cd0dc133c9bebfa8dee5e9b2f05989b87
Cornel Stoica <cornel.stoica@oit.edu>
2024-09-09 23:23:48 -0700

    Added main components. Navbar is good. Working on footer next.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+    Navbar,
+    Exchanges,
+    Homepage,
+    Cryptocurrencies,
+    News,
+    CryptoDetails,
+} from "./components";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route
+                                exact
+                                path="/"
+                                element={<Homepage />}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/exchanges"
+                                element={<Exchanges />}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/cryptocurrencies"
+                                element={<Cryptocurrencies />}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/crypto/:coinId"
+                                element={<CryptoDetails />}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/news"
+                                element={<News />}
+                            ></Route>
+                        </Routes>
+                    </div>
+                </Layout>
+            </div>
+            <div className="footer"></div>
+        </div>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
