394b604d98d461ba060193fb2f7602f03af8b7da
Cornel Stoica <cornel.stoica@oit.edu>
2025-05-11 02:35:06 -0700

    Further removed unecessary imports from jsx files

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import HTMLReactParser from "html-react-parser";
 import { useParams } from "react-router-dom";
 import millify from "millify";
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Col, Row, Typography } from "antd";
 import {
     Chart as ChartJS,
