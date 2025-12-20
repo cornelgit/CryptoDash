405fa96f7691f0aebaca24b1810813fbd9e61da7
Cornel Stoica <56093656+cornelgit@users.noreply.github.com>
2025-12-20 15:18:30 -0800

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
