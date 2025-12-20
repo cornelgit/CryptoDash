dd176df98738dcca069d0c8618209075c3be2086
Cornel Stoica <56093656+cornelgit@users.noreply.github.com>
2025-12-20 15:12:16 -0800

    Update vite.config.js to increase chunkSizeLimit per vercel recommendation

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+    // default is 500 KiB; adjust as needed
+    chunkSizeWarningLimit: 1500,
+    },
 });
